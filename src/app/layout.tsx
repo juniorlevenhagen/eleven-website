27fcd2b9c60a70195bf88aaea2a2be70e7e5cdf7
unknown <juniorlevenhagen@gmail.com>
2025-10-01 16:42:04 -0300

    feat: add vercel analytcs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -89,6 +90,7 @@ export default function RootLayout({
           Pular para o conteúdo principal
         </a>
         {children}
+        <Analytics />
       </body>
     </html>
   );
